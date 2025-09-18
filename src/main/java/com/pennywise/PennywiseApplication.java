0812134534bcfc14c4894ff2420d7fe6dfeffbd9
Suvidha248 <sreeramoju.suvidha@gmail.com>
2025-09-18 10:50:47 -0400

    Renamed the project

--- src/main/java/com/pennywise/PennywiseApplication.java
+++ src/main/java/com/pennywise/PennywiseApplication.java
@@ -1,13 +1,11 @@
-package com.example.demo;
+package com.pennywise;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
-
+public class PennywiseApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(PennywiseApplication.class, args);
 	}
-
 }
